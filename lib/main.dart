6c0c78a97a9b70621feb572fcaf74ccaf37ec989
Jesse <domingo.jesse@gmail.com>
2019-04-02 15:35:38 -0700

    maindart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,129 @@
 import 'package:flutter/material.dart';
-import 'home_widget.dart';
+import 'package:location/location.dart';
 
+void main() => runApp(App());
 
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Homepage',
+      title: 'Title',
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.red,
+          accentColor: Colors.pink
+      ),
       home: Home(),
     );
   }
+}
 
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+
+
+  int _selectedIndex = 0;
+  final _widgetOptions = [
+    Text('Index 0: Home'),
+    Text('Index 1: Message'),
+    Text('Index 2: Profile'),
+  ];
+  Location location = Location();
+
+  Map<String, double> currentLocation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    location.onLocationChanged().listen((value) {
+      setState(() {
+        currentLocation = value;
+      });
+    });
   }
 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title:
+          RichText(
+            text: TextSpan(
+              text: 'Looking 4 Love',
+              style: TextStyle(fontStyle : FontStyle.italic, color: Colors.lightGreen.withOpacity(0.1)),
+            ) ,
+          )
+      ),
+      body: Column( children: <Widget>[
+        //Row1
+        Row(
+            children: [
+              Container(
+                height: 200,
+                width: 360,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Image.network('https://picsum.photos/360/200'),
+                ),
+              ),
+            ]
+        ),
+        //Row2
+        Row(
+            children: [
+              Container(
+                height: 200,
+                width: 360,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Image.network('https://lorempixel.com/360/200'),
+                ),
+              ),
+            ]
+        ),
+        Row(children: <Widget>[
+          currentLocation == null
+              ? CircularProgressIndicator()
+              : Text("Location:" + currentLocation["latitude"].toString() + " " + currentLocation["longitude"].toString()),
+        ]
+        )
+        //     Center(
+        //     child:  _widgetOptions.elementAt(_selectedIndex),),
+      ]
+
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(icon: Icon(Icons.mail), title: Text('Message')),
+          BottomNavigationBarItem(icon: Icon(Icons.person), title: Text('Profile')),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.deepPurple,
+        onTap: _onItemTapped,
+      ),
+
+
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
 
